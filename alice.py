62b8f4d630c4a52b90bc739b9ab90ac5708a2570
Jack Jackson <jackjack@amazon.com>
2014-10-21 20:39:59 +0100

    Made Alice more concise

--- alice.py
+++ alice.py
@@ -2,7 +2,7 @@
 import json
 import random
 
-base_string = 'is the most fun a girl can have without taking her clothes off.'
+base_string = 'is the most fun a girl can have without taking her clothes off, but it\'s better if you do.'
 
 with file('verbs','r') as f:
     verbs = json.load(f)
